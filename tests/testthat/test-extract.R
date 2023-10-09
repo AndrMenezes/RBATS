8b00feea165d4d7291a66a2d5956de029a7d5009
AndrMenezes <andrefelipemaringa@gmail.com>
2023-10-09 10:20:09 +0100

    add test for lpl from extract method

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -1,12 +1,22 @@
 test_that("extract method for dlm objects", {
+  devtools::load_all()
 
   y <- c(AirPassengers)
   m <- dlm(polynomial = list(order = 2, discount_factor = 0.95),
            seasonal = list(type = "free", period = 12,
-                           harmonics = 1:3, discount_factor = 0.98))
+                           harmonics = 1:3, discount_factor = 0.98),
+           df_variance = 0.95)
   x <- fit(model = m, y = y,
            m0 = matrix(c(110, rep(0, 12)), ncol = 1),
            C0 = diag(1000, 13))
+  e_f <- extract(x = x, prob_interval = c(0.05, 0.20), distribution = "filter",
+                 component = "response")
+
+  expect_equal(
+    sum(log(1 / sqrt(e_f$variance) * dt(x = (e_f$y - e_f$mean) / sqrt(e_f$variance),
+                                        df = e_f$degrees_freedom))),
+    logLik(x)[1L]
+  )
 
   e_s <- extract(x = x, prob_interval = c(0.05, 0.20), distribution = "smooth",
                  component = "state")
